03537537278ef69f15057a9129b6e8127097a11b
sheng wei <shengwei@shengs-MacBook-Pro.local>
2017-03-13 00:20:14 -0400

    try http request

---
+++ generate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+data-plot.py: plot the average of every trace file
+"""
+
+
+"""
+__author__	= "Sheng Wei"
+__copyright__ = "Copyright 2016"
+__credits__ = "Sheng Wei"
+__email__ = "shengw12@vt.edu"
+"""
+
+
+import urllib2
+content = urllib2.urlopen("http://www.google.com").read()
